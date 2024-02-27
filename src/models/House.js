4ddd4e6d40ef9045dc47db37fc0bd417c7985177
Zac <step7476@alumni.uidaho.edu>
2024-02-27 15:56:25 -0700

    created House model, messed around with vue chrome extension to get a feel for it

---
+++ src/models/House.js
@@ -0,0 +1,28 @@
+export class House {
+    constructor(data) {
+        this.id = data.id || data._id
+        this.description = data.description
+        this.price = data.price
+        this.year = data.year
+        this.bedrooms = data.bedrooms
+        this.bathrooms = data.bathrooms
+    }
+
+}
+
+const houseData = {
+    // bathrooms: 2,
+    // bedrooms: 3,
+    // createdAt: "2023-05-11T21:41:07.979Z",
+    // creator: {_id: '63f7d6202d1cf882287f12e2', name: 'Charles Francis Xavier', picture: 'https://www.looper.com/img/gallery/professor-xs-entire-backstory-explained/intro-1587748942.jpg', id: '63f7d6202d1cf882287f12e2'},
+    // creatorId: "63f7d6202d1cf882287f12e2",
+    // description: "Super sick house",
+    // id: "645d60f381faf24223ae886b",
+    // imgUrl: "https://floorcentral.com/wp-content/uploads/2014/07/sick-house-syndrome.jpg",
+    // levels: 2,
+    // price: 230000,
+    // updatedAt: "2023-05-11T21:41:07.979Z",
+    // year: 2003,
+    // __v: 0,
+    // _id: "645d60f381faf24223ae886b"
+}

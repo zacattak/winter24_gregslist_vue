7a6efc71e530adccaf284510cc07f0d5788b2df9
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-27 11:10:56 -0700

    abstracted carcard to component

---
+++ src/components/CarCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <img :src="carProp.imgUrl" class="img-fluid" :alt="carProp.make + ' ' + carProp.model" role="button">
+  <div class="car-title">
+    <p class="fs-4 mb-1 ms-1">{{ carProp.make }} {{ carProp.model }}</p>
+  </div>
+</template>
+
+
+<script>
+import { computed } from 'vue';
+import { AppState } from '../AppState.js';
+import { Car } from '../models/Car.js';
+
+export default {
+  props: {
+    carProp: { type: Car, required: true }
+  },
+  setup() {
+    return {
+      // NOTE will only bring in first car
+      // car: computed(() => AppState.cars[0])
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+img {
+  height: 40vh;
+  width: 40vh;
+  border-radius: 16px;
+  object-fit: cover;
+  object-position: center;
+  box-shadow: 2px 3px 10px black;
+}
+
+
+.car-title {
+  position: absolute;
+  bottom: 0;
+  color: white;
+  text-shadow: 1px 1px 3px black;
+  font-weight: bold;
+}
+</style>
--- src/pages/CarsPage.vue
+++ src/pages/CarsPage.vue
@@ -8,10 +8,11 @@
 
     <section class="row">
       <div v-for="car in cars" :key="car.id" class="col-md-4 mb-3 car-card">
-        <img :src="car.imgUrl" class="img-fluid" :alt="car.make + ' ' + car.model">
+        <!-- <img :src="car.imgUrl" class="img-fluid" :alt="car.make + ' ' + car.model" role="button">
         <div class="car-title">
           <p class="fs-4 mb-1 ms-1">{{ car.make }} {{ car.model }}</p>
-        </div>
+        </div> -->
+        <CarCard :carProp="car" />
       </div>
     </section>
   </div>
@@ -23,52 +24,33 @@ import { computed, onMounted } from 'vue';
 import Pop from '../utils/Pop.js';
 import { carsService } from '../services/CarsService.js'
 import { AppState } from '../AppState.js'
+import CarCard from '../components/CarCard.vue';
 export default {
   setup() {
-
     async function getCars() {
       try {
-        await carsService.getCars()
-      } catch (error) {
-        Pop.error(error)
+        await carsService.getCars();
+      }
+      catch (error) {
+        Pop.error(error);
       }
     }
-
-
     // NOTE when the page loads!
     onMounted(() => {
       console.log('Page is mounted!');
-      getCars()
-    })
-
-
+      getCars();
+    });
     return {
       cars: computed(() => AppState.cars)
-    }
-  }
+    };
+  },
+  components: { CarCard }
 }
 </script>
 
 
 <style lang="scss" scoped>
-img {
-  height: 40vh;
-  width: 40vh;
-  border-radius: 16px;
-  object-fit: cover;
-  object-position: center;
-  box-shadow: 2px 3px 10px black;
-}
-
 .car-card {
   position: relative;
-}
-
-.car-title {
-  position: absolute;
-  bottom: 0;
-  color: white;
-  text-shadow: 1px 1px 3px black;
-  font-weight: bold;
 }
 </style>

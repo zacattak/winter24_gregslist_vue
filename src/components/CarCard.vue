4aaf811294ced21e2226fbb16a049455374d7af4
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-27 11:51:27 -0700

    added car id as route parameter in router link

--- src/components/CarCard.vue
+++ src/components/CarCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link :to="{ name: 'CarDetails' }">
+  <router-link :to="{ name: 'CarDetails', params: { carId: carProp.id } }">
     <img :src="carProp.imgUrl" class="img-fluid" :alt="carProp.make + ' ' + carProp.model" role="button">
     <div class="car-title">
       <p class="fs-4 mb-1 ms-1">{{ carProp.make }} {{ carProp.model }}</p>

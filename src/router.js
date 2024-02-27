ca0f1cd7ae3d2692a3a4842ce6fb339e27db895f
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-27 09:37:42 -0700

    added cars route object in router

--- src/router.js
+++ src/router.js
@@ -17,6 +17,11 @@ const routes = [
     component: loadPage('AboutPage')
   },
   {
+    path: '/cars',
+    name: 'Cars',
+    component: loadPage('CarsPage')
+  },
+  {
     path: '/account',
     name: 'Account',
     component: loadPage('AccountPage'),

6f7251718c83d7d2e5f068033c4d42e823eb001d
Zac <step7476@alumni.uidaho.edu>
2024-02-27 15:57:04 -0700

    added levels to model

--- src/models/House.js
+++ src/models/House.js
@@ -4,6 +4,7 @@ export class House {
         this.description = data.description
         this.price = data.price
         this.year = data.year
+        this.levels = data.levels
         this.bedrooms = data.bedrooms
         this.bathrooms = data.bathrooms
     }

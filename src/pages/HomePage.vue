019ab74d95c2eb584e3526c418ecb05f580f2c70
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-27 09:32:42 -0700

    removed default styling on home page

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container-fluid">
+    <section class="row">
+      <div class="col-12">
+        <h1 class="m-3">Welcome to Gregslist!</h1>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>

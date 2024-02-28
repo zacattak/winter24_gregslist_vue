892a3e5162ea1f8241d87f99efcd2d473e1b5fb4
Zac <step7476@alumni.uidaho.edu>
2024-02-28 09:13:45 -0700

    getting everything up to date thus far

---
+++ src/components/HouseCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <img src="houseProp.imgUrl" class="img-fluid" alt="house">
+</template>
+
+
+<script>
+import { computed } from 'vue';
+import { House } from '../models/House';
+import { Car } from '../models/Car';
+
+
+
+
+export default {
+    props: {
+        houseProp: { type: House, required: true }
+    },
+
+
+    setup() {
+        return {
+
+        }
+    }
+}
+</script>
+
+
+<style lang="scss" scoped></style>
